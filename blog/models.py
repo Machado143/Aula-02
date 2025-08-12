6e220af996ef916833984e3de39b24ac4b01de1f
Machado143 <machadogm14@gmail.com>
2025-08-12 10:35:31 -0300

    2

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    """
+    Esta é a classe que representa um post do blog.
+    vai possuir relação com :model:`auth.User`
+
+    """
+
+    autor = models.CharField(max_length=32) #campo texto
+    titulo = models.CharField(max_length=100)
+    conteudo = models.TextField()
+    data_postagem = models.DateTimeField(auto_now_add=True)
+
+    class Meta():
+        db_table = 'posts'  # nome da tabela no banco de dados
--- farmacia/settings.py
+++ farmacia/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'farmacia',  # Your app name
 ]
 
 MIDDLEWARE = [
