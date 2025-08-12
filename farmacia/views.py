2fb92bccdd3c6e4d61aa5a450953c85de366b891
Machado143 <machadogm14@gmail.com>
2025-08-12 12:43:22 -0300

    6

--- farmacia/views.py
+++ farmacia/views.py
@@ -4,4 +4,4 @@ from .models import Post
 # Create your views here.
 def inicio(request):
     meu_posts = Post.objects.all()
-    return render(request, 'inicio.html', {'posts': meu_posts})
+    return render(request, 'inicio.html', {'hahahahah': meu_posts})
